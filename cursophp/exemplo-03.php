8f47c83a78402c8d8e11638c4c215d400caa88ef
unknown <jmperes17@gmail.com>
2021-12-07 23:06:09 -0300

    Novos exemplos

--- cursophp/exemplo-03.php
+++ cursophp/exemplo-03.php
@@ -8,5 +8,31 @@ $nomeCompleto = $nome . " " . $sobrenome;
 
 echo $nomeCompleto;
 
+echo strtoupper($nomeCompleto);
+
+echo strtolower($nomeCompleto);
+
+echo "</br>";
+
+$idade = 17;
+$idadeCrianca = 12;
+$idadeAdulto = 18;
+$idadeMelhor = 65;
+
+if($idade >=18){
+    echo "Adulto";
+}
+elseif($idade <= 17 && $idade > 12){
+    echo "Adolescente";
+
+}
+else{
+    echo "idoso";
+}
+
+
+
+
+
 
 ?>
