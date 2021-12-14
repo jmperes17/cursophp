044129fbd8edac19007328eb69dec8addef19378
Jozé Junior <Joze Junior@franqueadora1.yes.com>
2021-12-14 17:49:49 -0300

    Array

---
+++ cursophp/exemplo-08.php
@@ -0,0 +1,19 @@
+<?php
+
+$frutas = array('Banana','Morango','Abacaxi');
+
+foreach($frutas as $fruta){
+    echo $fruta . ", ";
+}
+
+echo "<br>";
+
+print_r($frutas);
+
+
+
+
+
+
+
+?>
---
+++ cursophp/exemplo-09.php
@@ -0,0 +1,11 @@
+<?php
+
+
+$carros[0][0] = "Honda";
+$carros[0][1] = "Corolla";
+$carros[0][2] = "Civic";
+$carros[0][3] = "City";
+
+print_r($carros);
+
+?>
