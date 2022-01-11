e62e7d8afa8e88e10b9a9e1592b026b3f0be9ad1
unknown <jmperes17@gmail.com>
2022-01-11 01:24:36 -0300

    novos exemplos de funcoes

--- cursophp/exemplo-15.php
+++ cursophp/exemplo-15.php
@@ -1,6 +1,15 @@
 <?php
 
-echo "Olá mundo";
+    $pessoa = array(
+        'nome' => 'Junior',
+        'idade' => 25
+    );
+
+    foreach ($pessoa as $value){
+        if (gettype($value) === 'integer') $value += 1;
+        echo $value.'<br>';
+    }
+
 
 
 
