5a002502d2e0ceff3a255c44da55a9e7baa1959c
unknown <jmperes17@gmail.com>
2022-01-24 22:43:08 -0300

    new

---
+++ crud2/model/db.php
@@ -0,0 +1,12 @@
+<?php
+
+    $hostname="127.0.0.1";
+	$username="root";
+	$password="";
+	$dbname="crud2";
+	
+	$conexao = mysqli_connect($hostname,$username, $password, $dbname) or die ("html>script language='JavaScript'>alert('Unable to connect to database! Please try again later.'),history.go(-1)/script>/html>");
+
+    $query = ' SELECT * FROM usuarios; ';
+
+   
